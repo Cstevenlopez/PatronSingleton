c5cc5f4c0361e80e241e42ca03dd35e317789c19
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-10-22 11:26:06 -0600

    Terminacion de la clase DocumentaciónEnBlanco

--- src/main/java/org/steven/DocumentacionEnBlanco.java
+++ src/main/java/org/steven/DocumentacionEnBlanco.java
@@ -1,4 +1,21 @@
 package org.steven;
 
+import java.util.ArrayList;
+
 public class DocumentacionEnBlanco {
+
+    private static DocumentacionEnBlanco _instance = null;
+
+    private <Documento> DocumentacionEnBlanco()
+    {
+
+        ArrayList<Documento> documentos = new ArrayList<Documento>();
+    }
+
+    public static DocumentacionEnBlanco Instance()
+    {
+        if (_instance == null)
+            _instance = new DocumentacionEnBlanco();
+        return _instance;
+    }
 }

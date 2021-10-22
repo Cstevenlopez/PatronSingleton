e2a5e935912ea3980852eaf2e4e9722dda5d7f29
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-10-22 11:44:56 -0600

    Terminacion de la clase TestComercial donde mostraremos el nombre,direcccion,correo

--- src/main/java/org/steven/TestComercial.java
+++ src/main/java/org/steven/TestComercial.java
@@ -1,4 +1,19 @@
 package org.steven;
 
 public class TestComercial {
+    public static void main(String[] args)
+    {
+        Comercial poxscomerci= Comercial.Instance();
+        poxscomerci.setNombre("ZONACOOL");
+        poxscomerci.setDireccion("Colonea anexo a la primero de mayo");
+        poxscomerci.setEmail("Zonacoolmanagua@gmail.com");
+
+        visualiza();
+    }
+
+    public static void visualiza()
+    {
+        Comercial poxscomerci = Comercial.Instance();
+        poxscomerci.visualiza();
+    }
 }
